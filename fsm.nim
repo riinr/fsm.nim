f114873c65fad919c05928db0755e44697f19f4f
hugosenari <hugosenari@gmail.com>
2021-02-10 13:38:51 -0300

    chore: make it more verbose

--- fsm.nim
+++ fsm.nim
@@ -2,74 +2,73 @@ import tables, strutils, options
 
 type
   Callback = proc(): void
-  StateEvent[S,E] = tuple[state: S, event: E]
-  Transition[S] = tuple[nexState: S, action: Option[Callback]]
+  StateEvent[State, Event] = tuple[state: State, event: Event]
+  Transition[State] = tuple[nexState: State, action: Option[Callback]]
 
-  Machine[S,E] =  ref object of RootObj
-    initialState: S
-    currentState: Option[S]
-    transitions: TableRef[StateEvent[S,E], Transition[S]]
-    transitionsAny: TableRef[S, Transition[S]]
-    defaultTransition: Option[Transition[S]]
+  Machine[State, Event] =  ref object of RootObj
+    initialState: State
+    currentState: Option[State]
+    transitions: TableRef[StateEvent[State,Event], Transition[State]]
+    transitionsAny: TableRef[State, Transition[State]]
+    defaultTransition: Option[Transition[State]]
 
-  TransitionNotFoundException = object of Exception
+  TransitionNotFoundException = object of ValueError
 
-proc reset*(m: Machine) =
-  m.currentState = some(m.initialState)
+proc reset*(machine: Machine) =
+  machine.currentState = some(machine.initialState)
 
-proc setInitialState*[S,E](m: Machine[S,E], state: S) =
-  m.initialState = state
-  if m.currentState.isNone:
-    m.reset()
+proc setInitialState*[State, Event](machine: Machine[State, Event], state: State) =
+  machine.initialState = state
+  if isNone machine.currentState:
+    machine.reset()
 
-proc newMachine*[S,E](initialState: S): Machine[S,E] =
-  result = new(Machine[S,E])
-  result.transitions = newTable[StateEvent[S,E], Transition[S]]()
-  result.transitionsAny = newTable[S, Transition[S]]()
+proc newMachine*[State, Event](initialState: State): Machine[State, Event] =
+  result = new(Machine[State, Event])
+  result.transitions = newTable[StateEvent[State, Event], Transition[State]]()
+  result.transitionsAny = newTable[State, Transition[State]]()
   result.setInitialState(initialState)
 
-proc addTransitionAny*[S,E](m: Machine[S,E], state: S, nextState: S) =
-  m.transitionsAny[state] = (nextState, none(Callback))
+proc addTransitionAny*[State, Event](machine: Machine[State, Event], state: State, next: State) =
+  machine.transitionsAny[state] = (next, none(Callback))
 
-proc addTransitionAny*[S,E](m: Machine[S,E], state, nextState: S, action: Callback) =
-  m.transitionsAny[state] = (nextState, some(action))
+proc addTransitionAny*[State, Event](machine: Machine[State, Event], state, next: State, action: Callback) =
+  machine.transitionsAny[state] = (next, some(action))
 
-proc addTransition*[S,E](m: Machine[S,E], state: S, event: E, nextState: S) =
-  m.transitions[(state, event)] = (nextState, none(Callback))
+proc addTransition*[State, Event](machine: Machine[State,Event], state: State, event: Event, next: State) =
+  machine.transitions[(state, event)] = (next, none(Callback))
 
-proc addTransition*[S,E](m: Machine[S,E], state: S, event: E, nextState: S, action: Callback) =
-  m.transitions[(state, event)] = (nextState, some(action))
+proc addTransition*[State, Event](machine: Machine[State, Event], state: State, event: Event, next: State, action: Callback) =
+  machine.transitions[(state, event)] = (next, some(action))
 
-proc setDefaultTransition*[S,E](m: Machine[S,E], state: S) =
-  m.defaultTransition = some((state, none(Callback)))
+proc setDefaultTransition*[State, Event](machine: Machine[State, Event], state: State) =
+  machine.defaultTransition = some((state, none(Callback)))
 
-proc setDefaultTransition*[S,E](m: Machine[S,E], state: S, action: Callback) =
-  m.defaultTransition = some((state, some(action)))
+proc setDefaultTransition*[State, Event](machine: Machine[State, Event], state: State, action: Callback) =
+  machine.defaultTransition = some((state, some(action)))
 
-proc getTransition*[S,E](m: Machine[S,E], event: E, state: S): Transition[S] =
+proc getTransition*[State, Event](machine: Machine[State, Event], event: Event, state: State): Transition[State] =
   let map = (state, event)
-  if m.transitions.hasKey(map):
-    result = m.transitions[map]
-  elif m.transitionsAny.hasKey(state):
-    result = m.transitionsAny[state]
-  elif m.defaultTransition.isSome:
-    result = m.defaultTransition.get
+  if machine.transitions.hasKey(map):
+    result = machine.transitions[map]
+  elif machine.transitionsAny.hasKey(state):
+    result = machine.transitionsAny[state]
+  elif machine.defaultTransition.isSome:
+    result = machine.defaultTransition.get
   else: raise newException(TransitionNotFoundException, "Transition is not defined: Event($#) State($#)" % [$event, $state])
 
-proc getCurrentState*(m: Machine): auto =
-  m.currentState.get
+proc getCurrentState*(machine: Machine): auto =
+  machine.currentState.get
 
-proc process*[S,E](m: Machine[S,E], event: E) =
-  let transition = m.getTransition(event, m.currentState.get)
+proc process*[State, Event](machine: Machine[State, Event], event: Event) =
+  let transition = machine.getTransition(event, machine.currentState.get)
   if transition[1].isSome:
     get(transition[1])()
-  m.currentState = some(transition[0])
-  #echo event, " ", m.currentState.get
+  machine.currentState = some(transition[0])
 
 
 when isMainModule:
   proc cb() =
-    echo "i'm evaporating"
+    echo "I'm evaporating"
 
   type
     State = enum
@@ -84,16 +83,15 @@ when isMainModule:
       SUBLIMATE
       IONIZE
 
-  var m = newMachine[State, Event](LIQUID)
-  #m.setDefaultTransition()
-  m.addTransition(SOLID, MELT, LIQUID)
-  m.addTransition(LIQUID, EVAPORATE, GAS, cb)
-  m.addTransition(SOLID, SUBLIMATE, GAS)
-  m.addTransition(GAS, IONIZE, PLASMA)
-  m.addTransition(SOLID, MELT, LIQUID)
+  var machine = newMachine[State, Event](LIQUID)
+  machine.addTransition(SOLID, MELT, LIQUID)
+  machine.addTransition(LIQUID, EVAPORATE, GAS, cb)
+  machine.addTransition(SOLID, SUBLIMATE, GAS)
+  machine.addTransition(GAS, IONIZE, PLASMA)
+  machine.addTransition(SOLID, MELT, LIQUID)
 
-  assert m.getCurrentState() == LIQUID
-  m.process(EVAPORATE)
-  assert m.getCurrentState() == GAS
-  m.process(IONIZE)
-  assert m.getCurrentState() == PLASMA
+  assert machine.getCurrentState() == LIQUID
+  machine.process(EVAPORATE)
+  assert machine.getCurrentState() == GAS
+  machine.process(IONIZE)
+  assert machine.getCurrentState() == PLASMA
